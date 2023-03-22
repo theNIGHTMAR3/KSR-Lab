3fcf34ee1a64168d165500b9b54d221bda566e9b
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-03-22 16:30:20 +0100

    Finished Lab4

---
+++ Lab4/Lab4_klient/Program.cs
@@ -0,0 +1,50 @@
+﻿using KSR_WCF1;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // zad1
+            var fact = new ChannelFactory<IZadanie1>(new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost/ksr-wcf1-test"));
+
+            var client = fact.CreateChannel();
+
+            Console.WriteLine(client.Test("Testing 184631"));
+            try
+            {
+                client.RzucWyjatek(true);
+            }
+            catch(FaultException<Wyjatek> e)
+            {
+                Console.WriteLine(client.OtoMagia(e.Detail.magia));
+            }
+
+            // zad7
+            var client7 = new ServiceReference2.Zadanie7Client();
+            try
+            {
+                client7.RzucWyjatek7("a",7);
+            }
+            catch (FaultException<Wyjatek> e)
+            {
+                Console.WriteLine("Wyjatek7");
+            }
+
+
+
+            ((IDisposable)client).Dispose();
+            fact.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4/Lab4_server/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel
+using KSR_WCF1;
+using System.Runtime.Serialization;
+
+namespace Lab4_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var host = new ServiceHost(typeof(Zadanie2));
+
+            host.AddServiceEndpoint((typeof(IZadanie2)), 
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf1-zad2");
+
+            // zad3
+            var b = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (b == null)
+                b = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(b);
+            host.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName,
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                "net.pipe://localhost/metadata");
+
+            // zad4
+            host.AddServiceEndpoint(typeof(IZadanie2),
+                new NetTcpBinding(),
+                "nwt.tcp://127.0.0.1:55765");
+
+            // zad7
+            var host7=new ServiceHost(typeof(Zadanie7));
+            host7.AddServiceEndpoint(typeof(IZadanie7),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/metadata2");
+
+            var b7=host7.Description.Behaviors. Find<ServiceMetadataBehavior>();
+            if (b7 == null)
+                b7 = new ServiceMetadataBehavior();
+
+            host.Description.Behaviors.Add(b7);
+            host.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName,
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                "net.pipe://localhost/metadata2");
+
+
+
+            host.Open();
+
+            Console.ReadKey();
+
+            host.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    // zad2
+    [ServiceContract] public interface IZadanie2
+    {
+        [OperationContract] string Test(string arg);
+    }
+
+
+    public class Zadanie2 : IZadanie2
+    {
+        public string Test(string arg)
+        {
+            return $"You are testing with argument: {arg}";
+        }
+    }
+
+
+    // zad7
+    [ServiceContract]
+    public interface IZadanie7
+    {
+        [OperationContract][FaultContract(typeof(Wyjatek7))]
+
+        void RzucWyjatek7(string a, int b);
+    }
+
+
+    [DataContract]
+    public class Wyjatek7
+    {
+        [DataMember] public string Opis { get; set; }
+        [DataMember] public string A { get; set; }
+        [DataMember] public int B { get; set; }
+    }
+
+
+    public class Zadanie7 : IZadanie7
+    {
+        public void RzucWyjatek7(string a, int b)
+        {
+            var exception = new FaultException<Wyjatek7>(new Wyjatek7(),
+                new FaultReason("Wyrzucono wyjatek7 " + a + b));
+
+            throw exception;
+        }
+    }
+}
