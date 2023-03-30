f8c0bfb7b75d4e04e22d4acb5742261403a82b5b
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-03-30 10:47:52 +0200

    Finished Lab5

---
+++ Lab5/Lab5_5/Service1.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using KSR_WCF2;
+
+namespace Lab5_5
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IZadanie5, IZadanie6
+	{
+		public string ScalNapisy(string a, string b)
+		{
+			return a + b;
+		}
+
+		public void Dodaj(int a, int b)
+		{
+			var callback = OperationContext.Current.GetCallbackChannel<IZadanie6Zwrotny>();
+			callback.Wynik(a + b);
+		}
+	}
+}
---
+++ Lab5/Lab5__1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5__1
+{
+    public class Handler : sr1.IZadanie2Callback
+    {
+        public void Zadanie([MessageParameter(Name = "zadanie")] string zadanie1, int pkt, bool zaliczone)
+        {
+            Console.WriteLine($"{zadanie1} pkt: {pkt} zaliczone: {zaliczone}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // zad1 
+            var client1 = new sr1.Zadanie1Client();
+            IAsyncResult result = client1.DlugieObliczeniaAsync();
+
+
+            for (int x = 0; x < 21; x++)
+            {
+                Console.WriteLine(client1.Szybciej(x, 3 * x * x - 2 * x));
+            }
+            Console.ReadKey();
+
+            ((IDisposable)client1).Dispose();
+
+            var client2 = new sr1.Zadanie2Client(new InstanceContext(new Handler()));
+            client2.PodajZadania();
+            Console.ReadKey();
+
+            ((IDisposable)client2).Dispose();
+        }
+    }
+}
+
+
