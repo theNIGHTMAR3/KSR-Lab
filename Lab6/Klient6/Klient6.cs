99c2479892e241b7b146d86ba8bfb4c199d31acd
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2023-04-06 17:48:42 +0200

    Finished Lab6

---
+++ Lab6/Klient6/Klient6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Klient6
+{
+    [ServiceContract]
+    public interface IZadanie6
+    {
+        [OperationContract]
+        int Dodaj(int a, int b);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fabryka = new ChannelFactory<IZadanie6>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost/router"));
+            var klient = fabryka.CreateChannel();
+            Console.WriteLine(klient.Dodaj(13, 37));
+            Console.ReadKey();
+            ((IDisposable)klient).Dispose();
+            fabryka.Close();
+        }
+    }
+}
